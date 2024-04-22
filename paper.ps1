0c895daefe772a5741db37e5c7d9e3ffdf03825b
Pavel Tarasov <47327788+WhiteBeef@users.noreply.github.com>
2024-04-22 19:06:26 +0300

    Update paper.ps1

--- paper.ps1
+++ paper.ps1
@@ -6,25 +6,94 @@ param(
 )
 
 if($help){
-    echo 'Command: iex "&{$(irm https://p.whitebeef.ru/paper)} [-version <version>] [-port <port>] [-op]"'
+    Write-Output 'Command: iex "&{$(irm https://p.whitebeef.ru/paper)} [-version <version>] [-port <port>] [-op]"'
     return
 }
+
+$javaLatestVersion = 23
 
 $javaLinks = @{
     8 = "https://corretto.aws/downloads/latest/amazon-corretto-8-x64-windows-jre.zip"
     11 = "https://corretto.aws/downloads/latest/amazon-corretto-11-x64-windows-jdk.zip"
+    16 = "https://corretto.aws/downloads/latest/amazon-corretto-16-x64-windows-jdk.zip"
     17 = "https://corretto.aws/downloads/latest/amazon-corretto-17-x64-windows-jdk.zip"
     21 = "https://corretto.aws/downloads/latest/amazon-corretto-21-x64-windows-jdk.zip"
 }
 
+
+
+function javaRequired([string] $minecraftVersion) {
+    $minorMinecraftVersion = ($minecraftVersion -split "\.")[1]
+    if($null -eq ($minorMinecraftVersion -as [int])){
+        $minorMinecraftVersion = [int]($minorMinecraftVersion -split "-")[0]
+    } else {
+        $minorMinecraftVersion = $minorMinecraftVersion -as [int]
+    }
+    
+    if($minorMinecraftVersion -eq 0){
+        $error_message = "Minecraft minor version is not found"
+        Write-Host $error_message
+        return $null
+    }
+    $patchMinecraftVersion = [int]($minecraftVersion -split "\.")[2]
+    if($null -eq $patchMinecraftVersion){
+        $patchMinecraftVersion = 0
+    }
+
+    if(($minorMinecraftVersion -ge 8) -and ($minorMinecraftVersion -le 12)){
+        return @{
+            min_version = 8
+            max_version = $javaLatestVersion
+        }
+    }
+    if(($minorMinecraftVersion -ge 13) -and (($minorMinecraftVersion -le 16) -and ($patchMinecraftVersion -le 4))){
+        return @{
+            min_version = 11
+            max_version = 15
+        }
+    }
+    if(((($minorMinecraftVersion -ge 16) -and ($patchMinecraftVersion -ge 5)) -and ($minorMinecraftVersion -le 17)) -or (($minorMinecraftVersion -eq 17) -and ($patchMinecraftVersion -le 0))){
+        return @{
+            min_version = 16
+            max_version = 16
+        }
+    }
+    if((($minorMinecraftVersion -gt 17) -or (($minorMinecraftVersion -ge 17) -and ($patchMinecraftVersion -ge 1))) -and $minorMinecraftVersion -lt 19){
+        return @{
+            min_version = 16
+            max_version = $javaLatestVersion
+        }
+    }
+    if($minorMinecraftVersion -ge 19){
+        return @{
+            min_version = 17
+            max_version = $javaLatestVersion
+        }
+    }
+    return $null
+}
+
+function hasGui([string] $minecraftVersion) {
+    $minorMinecraftVersion = [int]($minecraftVersion -split "\.")[1]
+    if($minorMinecraftVersion -eq 0){
+        $error_message = "Minecraft minor version is not found"
+        Write-Host $error_message
+        return $false
+    }
+    $patchMinecraftVersion = [int]($minecraftVersion -split "\.")[2]
+    if(($minorMinecraftVersion -ge 16) -or (($minorMinecraftVersion -eq 15) -and ($patchMinecraftVersion -ge 2))){
+        return $true
+    }
+    return $false
+}
+
 if(($port -gt 65535) -or ($port -lt 1024)){
     $error_message = "Port must be specified in the range of 1024 to 65535"
-    $error_message
+    Write-Host $error_message
     return
 }
 
-$paperApi = 'https://api.papermc.io/v2/'
-$whitebeefVersionApi = 'https://whitebeef.ru/versions/'
+[string]$paperApi = 'https://api.papermc.io/v2/'
 $whitebeefMavenUrl = 'https://mvn.whitebeef.ru/releases/ru/nikita51/AutoUPnPPortOpen/'
 
 $existing_versions = ((Invoke-WebRequest $paperApi'projects/paper').content | Out-String | ConvertFrom-Json).versions
@@ -33,78 +102,85 @@ if (!$version) {
 }
 if(!($existing_versions -contains $version)){
     $error_message = "Version minecraft `""+$version+"`" does not exist, perhaps you meant: "+$existing_versions[-1]
-    $error_message
+    Write-Host $error_message
     return
 }
+
+# Write-Host $version
+
 $javaCommand = $null
 $installedJavaVersion = if($null -eq (Get-Command java -ErrorAction SilentlyContinue)) {$null} else {(Get-Command java | Select-Object -ExpandProperty Version).major}
-$minecraftJavaJson = (Invoke-WebRequest $whitebeefVersionApi$version).content | Out-String | ConvertFrom-Json
-$supportJavaVersions = $minecraftJavaJson.supportedVersions | Sort-Object -Descending
-if(($null -eq $installedJavaVersion) -Or !($supportJavaVersions -contains $installedJavaVersion)) {
-    $success = $False
-    foreach ($javaVersion in $supportJavaVersions)
-    {
-        $command = Get-Command -Name java$javaVersion -ErrorAction SilentlyContinue
-        if($command){
+# Write-Host $installedJavaVersion
 
-            $javaCommand = "java$javaVersion"
+$minecraftJavaRequired = javaRequired($version)
+if($null -eq $minecraftJavaRequired) {
+    return
+}
+
+if(($null -eq $installedJavaVersion) -or ($minecraftJavaRequired.min_version -gt $installedJavaVersion) -or ($minecraftJavaRequired.max_version -lt $installedJavaVersion)) {
+    $success = $False
+    for ($i=$minecraftJavaRequired.min_version; $i -le $minecraftJavaRequired.max_version; $i++) {
+        $command = Get-Command -Name java$i -ErrorAction SilentlyContinue
+        if($command){
+            $javaCommand = "java$i"
             $success = $True
             break
         }
     }
     if(!$success){
         if($null -eq $installedJavaVersion){
-            $error_message = "Java missing: Server requires a java version "+$minecraftJavaJson.preferredVersion+", but it's not installed"
+            $error_message = "Java missing: Server requires a java version "+$minecraftJavaRequired.min_version+"-"+$minecraftJavaRequired.max_version+", but it's not installed"
         } else {
-            $error_message = "Incorrect java version: installed '"+$installedJavaVersion+"', but requires '"+$minecraftJavaJson.preferredVersion+"'"
+            $error_message = "Incorrect java version: installed '"+$installedJavaVersion+"', but requires '"+$minecraftJavaRequired.min_version+"-"+$minecraftJavaRequired.max_version+"'"
         }
-        $error_message
-        $confirmation = Read-Host "Do you want to install the required version of java? (y/n)"
-        if (!($confirmation -eq 'y')) {
-            return
-        }
+        Write-Host $error_message
         $success = $False
-        foreach ($javaVersion in $supportJavaVersions)
-        {
-            if($javaLinks.ContainsKey($javaVersion)){
-
-                $link = $javaLinks[$javaVersion]
-                $javaFilename = "java"+$javaVersion+".zip"
-                $link
-
-                $ProgressPreference = 'SilentlyContinue'
-                Invoke-WebRequest -Uri $link -OutFile $javaFilename
-                $ProgressPreference = 'Continue'
-
-                # Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "echo chleeeeeen; pause"' -Verb RunAs -WindowStyle Hidden -Wait
-                $localJavaCommand = "java$javaVersion"
-                $command = 'pause'
-                $command = "`$global:ProgressPreference = `"SilentlyContinue`" ; Expand-Archive $((Get-Location).toString())\$javaFilename -DestinationPath `"$Env:Programfiles\Java\jdk-$javaVersion`" -Force; Remove-Item -Path $((Get-Location).toString())\$javaFilename -Force; `$global:ProgressPreference = `"Continue`";  echo Extracted;Set-Location -Path `"$Env:Programfiles\Java\jdk-$javaVersion`"; if((Get-ChildItem -Path (Get-Location).toString() -Directory -Force).Count -eq 1){`$jdkFolder = (Get-ChildItem -Path (Get-Location).toString() -Directory)[0].Name; Set-Location `$jdkFolder; Get-ChildItem -Path (Get-Location).toString() | Move-Item -Destination (Get-Item (Get-Location).toString()).Parent.FullName -Force; cd ..; Remove-Item `$jdkFolder -Force} ;Set-Location bin; if(Test-Path java.exe){ Copy-Item -Path `"java.exe`" -Destination `"$localJavaCommand.exe`" }; `$env:PATH -split `";`"; if(!(`$env:PATH -split `";`" -contains (Get-Location).Path)){`$Path = [Environment]::GetEnvironmentVariable(`"PATH`", `"Machine`") + [IO.Path]::PathSeparator + (Get-Location).Path.toString(); [Environment]::SetEnvironmentVariable( `"Path`", `$Path, `"Machine`" ); echo `"Java folder added to Path`"}"
-                Start-Process powershell -Wait -Verb RunAs -ArgumentList `
-      "-Command & { $($command -replace '"', '\"') } '$PSScriptRoot'"
-
-
-                $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-                if(Get-Command -Name $localJavaCommand){
-                    Write-Output ("Java version "+$javaVersion+" has been successfully installed and can be accessed by name: "+$localJavaCommand)
-                    $javaCommand = $localJavaCommand
-                    $success = $True
-                    break
-                } else {
-                    Write-Warning "Java version" $javaVersion "hasn't been installed"
-                    $env:Path
-                }
-
+        foreach($i in ($javaLinks.Keys | Sort-Object)){
+            if($i -lt $minecraftJavaRequired.min_version){
+                continue
             }
+            if($i -gt $minecraftJavaRequired.max_version){
+                continue
+            }
+            $link = $javaLinks[$i]
+            $confirmation = Read-Host "Do you want to install the "+$i+" version of java? (y/n/skip)"
+            if ($confirmation -eq 'skip' -and !($null -eq $installedJavaVersion)) {
+                $javaCommand = "java"
+                $success = $true
+                break
+            }
+            if (!($confirmation -eq 'y')) {
+                return
+            }
+            $javaFilename = "java"+$i+".zip"
 
+            $ProgressPreference = 'SilentlyContinue'
+            Invoke-WebRequest -Uri $link -OutFile $javaFilename
+            $ProgressPreference = 'Continue'
+            
+            $localJavaCommand = "java$i"
+            $command = 'pause'
+            $command = "`$global:ProgressPreference = `"SilentlyContinue`" ; Expand-Archive $((Get-Location).toString())\$javaFilename -DestinationPath `"$Env:Programfiles\Java\jdk-$i`" -Force; Remove-Item -Path $((Get-Location).toString())\$javaFilename -Force; `$global:ProgressPreference = `"Continue`";  echo Extracted;Set-Location -Path `"$Env:Programfiles\Java\jdk-$i`"; if((Get-ChildItem -Path (Get-Location).toString() -Directory -Force).Count -eq 1){`$jdkFolder = (Get-ChildItem -Path (Get-Location).toString() -Directory)[0].Name; Set-Location `$jdkFolder; Get-ChildItem -Path (Get-Location).toString() | Move-Item -Destination (Get-Item (Get-Location).toString()).Parent.FullName -Force; cd ..; Remove-Item `$jdkFolder -Force} ;Set-Location bin; if(Test-Path java.exe){ Copy-Item -Path `"java.exe`" -Destination `"$localJavaCommand.exe`" }; `$env:PATH -split `";`"; if(!(`$env:PATH -split `";`" -contains (Get-Location).Path)){`$Path = [Environment]::GetEnvironmentVariable(`"PATH`", `"Machine`") + [IO.Path]::PathSeparator + (Get-Location).Path.toString(); [Environment]::SetEnvironmentVariable( `"Path`", `$Path, `"Machine`" ); echo `"Java folder added to Path`"}"
+            Start-Process powershell -Wait -Verb RunAs -ArgumentList `
+    "-Command & { $($command -replace '"', '\"') } '$PSScriptRoot'"
+
+            $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+            if(Get-Command -Name $localJavaCommand){
+                Write-Output ("Java version "+$i+" has been successfully installed and can be accessed by name: "+$localJavaCommand)
+                $javaCommand = $localJavaCommand
+                $success = $True
+                break
+            } else {
+                Write-Warning "Java version" $i "hasn't been installed"
+                $env:Path
+            }
         }
         if(!$success){
-            $error_message = "No link to install one of the supported java versions for this version of minecraft, install java '"+$minecraftJavaJson.preferredVersion+"' yourself"
-            $error_message
+            $error_message = "No link to install one of the supported java versions for this version of minecraft, install java '"+$minecraftJavaRequired.min_version+"-"+$minecraftJavaRequired.max_version+"' yourself"
+            Write-Host $error_message
             return
         }
     }
-
 } else {
     $javaCommand = 'java'
 }
@@ -127,8 +203,7 @@ $ProgressPreference = 'Continue'
 $minecraftVersion = [int](($version.Split('.') | Select-Object -SkipLast 1) -join "")
 $ramValue = ([Math]::Min([Math]::Max([Math]::Floor((Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb /2),2),8)).toString()
 $startFileText = $javaCommand+" -Xms"+$ramValue+"G -Xmx"+$ramValue+"G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -jar $filename"
-# if($minecraftVersion -gt 114){
-if($minecraftJavaJson.hasGui){
+if(hasGui($version)){
     $startFileText += " --nogui"
 }
 $startFileText += " & pause"
@@ -142,12 +217,12 @@ if($openPort){
     New-Item -ItemType Directory -Force -Path $pluginPath | Out-Null
     Push-Location 'plugins'
     $url = $whitebeefMavenUrl+"maven-metadata.xml"
-    $data = [xml](iwr $url).content
+    $data = [xml](Invoke-WebRequest $url).content
     $pluginName = $data.metadata.artifactId
     $pluginVersion = $data.metadata.versioning.latest
     $pluginUrl = $whitebeefMavenUrl+$pluginVersion+'/'+$pluginName+'-'+$pluginVersion+'.jar'
     Invoke-WebRequest -Uri $pluginUrl -OutFile PortOpener.jar
     Pop-Location
 }
-start start.bat
+Start-Process start.bat
 Pop-Location
